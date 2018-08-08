e7c32a3b0e1aadd8130928bf890836468b095465
gaaamii <i1shi4ga4mi1@gmail.com>
2018-08-09 01:38:46 +0900

    preventDefault on a link, and handle PageChhange event manually

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img, p)
-import Html.Attributes exposing (src)
+import Html exposing (Html, text, div, h1, img, p, a, ul, li)
+import Html.Attributes exposing (src, href)
+import Html.Events exposing (onClick, onWithOptions, defaultOptions)
 import Navigation
+import Json.Decode
 
 
 ---- MODEL ----
@@ -23,25 +25,42 @@ init location =
 
 type Msg
     = UrlChange Navigation.Location
-
+    | PageChange String
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     UrlChange location ->
-      ( model, Cmd.none )
+      ( { model | location = location }, Cmd.none )
 
+    PageChange page ->
+      ( model, Navigation.newUrl page )
 
 
 ---- VIEW ----
 
+onPageChange msg =
+    onWithOptions
+        "click"
+        { defaultOptions | preventDefault = True }
+        (Json.Decode.succeed msg)
+
+linkTo : String -> String -> Html Msg
+linkTo url linkText =
+  a [ href url, onPageChange (PageChange url) ] [ text linkText ]
 
 view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , h1 [] [ text "SPAの練習" ]
         , p [] [ text model.location.href ]
+        , ul []
+          [
+            li [] [ linkTo "/a" "Aページ" ]
+            , li [] [ linkTo "/b" "Bページ" ]
+            , li [] [ linkTo "/c" "Cページ" ]
+          ]
         ]
 
 
