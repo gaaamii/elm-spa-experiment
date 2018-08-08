7690439ef5a2fac1ddeab365735c4ae0d11aaa95
gaaamii <i1shi4ga4mi1@gmail.com>
2018-08-09 02:52:56 +0900

    add router

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img, p, a, ul, li)
+import Html exposing (Html, text, div, h1, h2, img, p, a, ul, li, span)
 import Html.Attributes exposing (src, href)
 import Html.Events exposing (onClick, onWithOptions, defaultOptions)
 import Navigation
+import UrlParser exposing (..)
 import Json.Decode
 
 
@@ -11,12 +12,12 @@ import Json.Decode
 
 
 type alias Model =
-    { location : Navigation.Location }
+    { route : Maybe Route }
 
 
 init : Navigation.Location -> ( Model, Cmd Msg )
 init location =
-    ( { location = location }, Cmd.none )
+    ( { route = parsePath router location }, Cmd.none )
 
 
 
@@ -31,11 +32,29 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     UrlChange location ->
-      ( { model | location = location }, Cmd.none )
+      let
+          currentRoute =
+              parsePath router location
+      in
+      ( { model | route = currentRoute }, Cmd.none )
+
 
     PageChange page ->
       ( model, Navigation.newUrl page )
 
+---- Router ----
+type Route
+  = Home
+  | Blog Int
+  | User String
+
+router : Parser (Route -> a) a
+router =
+  oneOf
+    [ map Home top
+    , map Blog    (s "blog" </> int)
+    , map User    (s "user" </> string)
+    ]
 
 ---- VIEW ----
 
@@ -54,16 +73,46 @@ view model =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "SPAの練習" ]
-        , p [] [ text model.location.href ]
+        , viewCurrentPage model
         , ul []
           [
-            li [] [ linkTo "/a" "Aページ" ]
-            , li [] [ linkTo "/b" "Bページ" ]
-            , li [] [ linkTo "/c" "Cページ" ]
+            li [] [ linkTo "/" "Home" ]
+            , li [] [ linkTo "/blog/123" "Blog" ]
+            , li [] [ linkTo "/user/hogehoge" "User" ]
           ]
         ]
 
+viewCurrentPage : Model -> Html Msg
+viewCurrentPage model =
+    case model.route of
+        Nothing ->
+            viewHome
 
+        Just Home ->
+            viewHome
+
+        Just (User userName) ->
+            viewUser userName
+
+        Just (Blog id) ->
+            viewBlog id
+
+viewHome =
+    h2 [] [ text "ホームです" ]
+
+viewBlog id =
+    div [] [
+      h2 [] [ text "ブログです" ]
+      , p [] [
+          span [] [ text <| "ID:" ++ (toString id) ]
+        ]
+    ]
+
+viewUser userName =
+    div [] [
+      h2 [] [ text "ユーザーです" ]
+      , p [] [ text <| "ユーザー名：" ++ userName ]
+    ]
 
 ---- PROGRAM ----
 
